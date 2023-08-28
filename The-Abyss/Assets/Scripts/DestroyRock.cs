5762e8a7c2d10983a21b52c32eb11bcd31c5b071
PedroDepa <pedro.depauli22@gmail.com>
2023-08-28 06:12:34 -0300

    Assets completados

--- The-Abyss/Assets/Scripts/DestroyRock.cs
+++ The-Abyss/Assets/Scripts/DestroyRock.cs
@@ -8,17 +8,21 @@ public class DestroyRock : MonoBehaviour
     [SerializeField] private float radioHit  = 1.15f; 
     [SerializeField] private KeyCode buttonHit = KeyCode.E; 
 
+
     private void Update()
     {
         if (Input.GetKeyDown(buttonHit))
         {
             VerifyRockToDestroy();
+            
         }
     }
 
     private void VerifyRockToDestroy()
     {
         Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, radioHit);
+
+        
 
         foreach (Collider2D collider in colliders)
         {
