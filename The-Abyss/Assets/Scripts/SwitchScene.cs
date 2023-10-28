bbd3d523084cf0cf2236221e2b2bd1c5386789c7
PedroDepa <pedro.depauli22@gmail.com>
2023-10-28 04:09:50 -0300

    story agregada

--- The-Abyss/Assets/Scripts/SwitchScene.cs
+++ The-Abyss/Assets/Scripts/SwitchScene.cs
@@ -8,7 +8,7 @@ public class SwitchScene : MonoBehaviour
   
     private void Start()
     {
-        
+       
     }
   
     public void Play()
---
+++ The-Abyss/Assets/Scripts/Texts.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+[System.Serializable]
+
+public class Texts : MonoBehaviour
+{
+    [TextArea (2,6)]
+
+    public string[] arrayTexts;
+
+    [SerializeField] TextMeshProUGUI dialogueText;
+
+    private bool didDialogueStart;
+
+    private int lineIndex;
+
+    [SerializeField] private GameObject scene;
+
+    //private int countText = 0; 
+
+    void Start()
+    {
+        
+        StartDialogue();
+        
+    }
+
+    private void StartDialogue(){
+
+        didDialogueStart = true;
+        lineIndex = 0;
+        StartCoroutine(ShowLine());
+    }
+
+    private void NextDialogueLine(){
+
+        lineIndex++;
+
+        if(lineIndex < arrayTexts.Length){
+
+            StartCoroutine(ShowLine());
+        }
+        else{
+            didDialogueStart = false;
+
+            this.gameObject.SetActive(false);
+            scene.SetActive(true);
+        }
+
+    }
+
+    private IEnumerator ShowLine (){
+
+        dialogueText.text = "";
+        foreach(char caracter in arrayTexts[lineIndex]){
+
+            dialogueText.text += caracter;
+            yield return new WaitForSeconds(0.02f);
+        }
+    }
+    
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E)){
+
+              if(!didDialogueStart){ 
+
+                
+
+                StartDialogue();
+
+            }
+            else if(dialogueText.text == arrayTexts[lineIndex]){
+                NextDialogueLine();
+                
+            }
+            else{
+                StopAllCoroutines();
+                dialogueText.text = arrayTexts[lineIndex];
+            }
+        }
+
+
+        /*if(countText == 4){
+
+            SceneManager.LoadScene(2);
+        }
+        */
+    }
+}
