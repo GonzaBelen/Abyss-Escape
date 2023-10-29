c51897584d147472e288fc3424223e2213bb3444
MaxiRava <maximoravazzani@gmail.com>
2023-10-29 05:05:14 -0300

    arreglo de bugs

--- The-Abyss/Assets/Scripts/Controls.cs
+++ The-Abyss/Assets/Scripts/Controls.cs
@@ -35,22 +35,28 @@ public class Controls : MonoBehaviour
         if (collider.gameObject.CompareTag("Box Jump"))
         {
             director[1].SetActive(true);
+            Destroy(director[0]);
+
             Destroy(controls[1]);
             Destroy(collider);
-            //controls[2].SetActive(true);
+            
         }
 
         if (collider.gameObject.CompareTag("Box Run"))
         {
             director[2].SetActive(true);
+            Destroy(director[1]);
+
             Destroy(controls[2]);
             Destroy(collider);
 
         }
 
-        if (collider.gameObject.CompareTag("Box Rock2"))
+        if (collider.gameObject.CompareTag("Box Fairy"))
         {
             director[5].SetActive(true);
+            Destroy(director[4]);
+
             Destroy(controls[4]);
             //controls[3].SetActive(true);
             Destroy(collider);
@@ -59,14 +65,25 @@ public class Controls : MonoBehaviour
         if (collider.gameObject.CompareTag("Box Rock"))
         {
             director[3].SetActive(true);
-            
+            Destroy(director[2]);
+
             Destroy(collider);
             //controls[4].SetActive(true);
+        }
+
+        if (collider.gameObject.CompareTag("Box Rock2"))
+        {
+
+            controls[3].SetActive(false);
+            Destroy(collider);
+            
         }
 
         if (collider.gameObject.CompareTag("Box Grapple"))
         {
             director[4].SetActive(true);
+            Destroy(director[3]);
+
             Destroy(controls[3]);
             Destroy(collider);
         }
